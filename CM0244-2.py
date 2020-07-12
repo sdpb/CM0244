9d9d2fea609a61ddae88dada74a8107309d33779
sdpb <sdpalaciob@eafit.edu.co>
2020-07-11 23:31:56 -0500

    Moviendo algunos métodos para limpiar el código

--- CM0244-2.py
+++ CM0244-2.py
@@ -1,10 +1,15 @@
+# Bibliotecas
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
 from scipy.stats import anderson, t
 from statsmodels.api import OLS, qqplot
+import matplotlib.pyplot as plt
+
+# Locales
+from utilidades.Basicos import *
+from utilidades.Graficas import *
 
 # Primero importamos nuestra base de datos de saber 11
 dataSet = pd.read_csv('saber.csv')
@@ -22,46 +27,12 @@ materias = ['puntaje_lectura', 'puntaje_matematicas',
             'puntaje_sociales', 'puntaje_naturales', 'puntaje_ingles']
 
 
-# Filtra el dataset dado un atributo de datos y un parámetro condicional igual o diferente
-def filtro_igual(dataS, atributo_muestra, atributo_param, bool_diferente):
-    if bool_diferente:
-        return dataS[atributo_muestra != atributo_param]
-    else:
-        return dataS[atributo_muestra == atributo_param]
-
-
-# Filtra el dataset dado un atributo de datos y un parámetro condicional mayor o igual
-def filtro_mayor(dataS, atributo_muestra, atributo_param, bool_mayor_igual):
-    if bool_mayor_igual:
-        return dataS[dataS[atributo_muestra] >= atributo_param]
-    else:
-        return dataS[dataS[atributo_muestra] > atributo_param]
-
-
-# Filtra el dataset dado un atributo de datos y un parámetro condicional menor o igual
-def filtro_menor(dataS, atributo_muestra, atributo_param, bool_menor_igual):
-    if bool_menor_igual:
-        return dataS[dataS[atributo_muestra] <= atributo_param]
-    else:
-        return dataS[dataS[atributo_muestra] < atributo_param]
-
-
 # Retorna una muestra dado una función de filtro, su parámetro respectivo
 # y un porcentaje como tamaño de la muestra
 def obtener_muestra(dataS, filtro_muestra, filtro_param, tam_muestra):
     muestra = filtro_igual(dataS, filtro_muestra, filtro_param, False)
     muestra = muestra.sample(frac=tam_muestra)
     return muestra
-
-
-# Retorna la media o promedio de un atributo de una muestra (posiblemente la población)
-def obtener_media(atributo_muestra):
-    return np.nanmean(atributo_muestra).round(4)
-
-
-# Halla Porcentaje o media relativa dados los parámetros de la muestra y la población
-def porcentaje(total_poblacion, total_muestras):
-    return (total_muestras / total_poblacion) * 100
 
 
 # Retorna una lista que contiene la frecuencia relativa de cada tipo de casos favorables
@@ -75,27 +46,12 @@ def porcentaje_tipo(tam_poblacion, casos_favorables, atributo_casos_fav):
     return datos, etiquetas, atributo_casos_fav
 
 
-# Retorna la desviación estándar de un atributo de una muestra (posiblemente la población)
-def obtener_desvEst(atributo_muestra):
-    return np.nanstd(atributo_muestra).round(4)
-
-
 # Retorna un resumen del atributo de una muestra (media, desviación estándar y tamaño de la muestra)
 def resumen(atributo_muestra):
     media = obtener_media(atributo_muestra)
     desviacion_estandar = obtener_desvEst(atributo_muestra)
     longitud_muestra = len(atributo_muestra)
     print("Media: {}\nDesviación estándar: {}\nTamaño muestra: {}".format(media, desviacion_estandar, longitud_muestra))
-
-
-def tarta(datos, etiquetas, titulo):
-    # Diagrama de torta
-    colores = ['gold', 'lightcoral', 'lightskyblue']
-    plt.pie(datos, labels=etiquetas, colors=colores,
-            autopct='%1.1f%%', shadow=False, startangle=140)
-    plt.title(titulo)
-    plt.axis('equal')
-    plt.show()
 
 
 def formula(x, s, u_0, long_m):
