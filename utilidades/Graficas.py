237392e50ee99f56574b6d4d9017fdda20de3872
sdpb <sdpalaciob@eafit.edu.co>
2020-07-12 11:09:52 -0500

    Se añade nuevo método para graficación del modelo lineal

--- utilidades/Graficas.py
+++ utilidades/Graficas.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
 
 def tarta(datos, etiquetas, titulo):
@@ -11,3 +12,13 @@ def tarta(datos, etiquetas, titulo):
     plt.show()
 
 
+def plot_regresion(x, y, etiqueta_x, etiqueta_y, y_pred=None):
+    plt.scatter(x, y, s=np.pi * 3, alpha=0.5)
+    plt.xlabel('{}'.format(etiqueta_x.upper()))
+    plt.ylabel('{}'.format(etiqueta_y.upper()))
+    if y_pred is not None:
+        plt.plot(x, y_pred, color='red')
+        plt.title('MODELO DE REGRESIÓN')
+    else:
+        plt.title('GRÁFICO DE DISPERSIÓN')
+    plt.show()
