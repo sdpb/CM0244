4655ee6d69d53e40ff642aa201a4fd5c19df67f9
sdpb <sdpalaciob@eafit.edu.co>
2020-07-22 18:34:14 -0500

    Añadiendo métodos auxiliares

--- utilidades/Basicos.py
+++ utilidades/Basicos.py
@@ -1,12 +1,21 @@
-import numpy as np
+# Bibliotecas
+from numpy import nanmean, nanstd, nanpercentile
+from statistics import median
+
+# Locales
+from main import DATASET
 
 
 # Filtra el dataset dado un atributo de datos y un parámetro condicional igual o diferente
 def filtro_igual(dataS, atributo_muestra, atributo_param, bool_diferente):
     if bool_diferente:
-        return dataS[atributo_muestra != atributo_param]
+        return dataS[dataS[atributo_muestra] != atributo_param]
     else:
-        return dataS[atributo_muestra == atributo_param]
+        return dataS[dataS[atributo_muestra] == atributo_param]
+
+
+def filtro_periodo(periodo):
+    return filtro_igual(DATASET, 'año_semestre', periodo, False)
 
 
 # Filtra el dataset dado un atributo de datos y un parámetro condicional mayor o igual
@@ -27,7 +36,11 @@ def filtro_menor(dataS, atributo_muestra, atributo_param, bool_menor_igual):
 
 # Retorna la media o promedio de un atributo de una muestra (posiblemente la población)
 def obtener_media(atributo_muestra):
-    return np.nanmean(atributo_muestra).round(4)
+    return nanmean(atributo_muestra).round(4)
+
+
+def obtener_mediana(atributo_muestra):
+    return median(atributo_muestra)
 
 
 # Halla Porcentaje o media relativa dados los parámetros de la muestra y la población
@@ -37,4 +50,14 @@ def porcentaje(total_poblacion, total_muestras):
 
 # Retorna la desviación estándar de un atributo de una muestra (posiblemente la población)
 def obtener_desvEst(atributo_muestra):
-    return np.nanstd(atributo_muestra).round(4)
+    return nanstd(atributo_muestra).round(4)
+
+
+# Retorna la frecuencia de algún atributo que cumpla con una condición
+def obtener_frecuencia(atributo_muestra):
+    return len(atributo_muestra)
+
+
+def obtener_cuartiles(atributo_muestra):
+    return [nanpercentile(atributo_muestra, 25), nanpercentile(atributo_muestra, 50),
+            nanpercentile(atributo_muestra, 75)]
